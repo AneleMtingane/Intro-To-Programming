f37d7aa8bcf9ffd46de0628ffd3976b46265a533
Anele Mtingane <139566435+4n313@users.noreply.github.com>
2023-11-20 19:15:07 +0200

    Add files via upload

---
+++ KidsBook/Chapter1/Chapter 2.js
@@ -0,0 +1,215 @@
+
+
+    // console.log(false && false);
+    // console.log("This is a long string".slice(0, 4));
+    // console.log(99 * 123);
+    // console.log(12345 + 56789);
+    // console.log(1000 - 17);
+    // console.log(1234 + 57 * 3 - 31 / 4);
+    // console.log(8 / 1 + 3);
+    // console.log(8 / (1 + 3));
+
+    // // //Try It Out page 44
+    // console.log((15 + 9) * 2);
+// //Variables
+
+// let age;
+// age = 12;
+
+// //console.log(age);
+
+// // let age =12;
+
+// let name;
+
+// // age =13;
+
+// console.log(age);
+// console.log(name);
+
+// name = "Anele";
+// console.log(name);
+
+// let numberOfSiblings = 1 + 3;
+// let numberOfCandies = 8;
+// console.log(numberOfCandies / numberOfSiblings);
+
+// let secondsInAMinute = 60;
+// let minutesInAnHour = 60;
+// secondsInAnHour = secondsInAMinute * minutesInAnHour;
+// console.log(secondsInAnHour);
+
+// let hoursInaDay = 24;
+// let secondsInADay = secondsInAnHour * hoursInaDay
+// console.log(secondsInADay);
+
+// let daysInAYear = 365;
+// let secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
+
+// let age = 30;
+// let secondsInMyAge = secondsInAYear * age;
+// console.log("I am " + secondsInMyAge + " seconds old!");
+// console.log(`I am ${secondsInMyAge} seconds old`)*/
+
+// Incrementing and Decrementing
+//Post Incrementing and Decrementing
+
+// let highFives = 0;
+// ++highFives;
+// console.log(`high fives is ${highFives}`);
+// ++highFives;
+// console.log(`high fives is ${highFives}`);
+// --highFives;
+// console.log(`high fives is ${highFives}`);
+
+//Pre Incrementing and decrementing
+
+// highFives = 0;
+// console.log(highFives);
+// highFives++;
+// console.log(highFives);
+// highFives++;
+// console.log(highFives);
+// highFives--;
+// console.log(highFives);
+//console.log(highFives);
+
+//Plus equals and minus equals
+
+// let x = 10;
+// x = x+5;
+// console.log(x);
+
+// x = 10;
+// x+=20;
+// console.log(x);
+
+// let score = 10;
+// score-=7;
+// console.log(score);
+
+// score-=3;
+// console.log(score);
+
+// //Try It Out
+
+// let balloons = 100;
+// balloons*=2;
+// console.log(balloons);
+
+// let balloons2 = 100;
+// balloons2 /=4;
+// console.log(balloons2);
+
+// Strings
+
+// let numberNine = 9;
+// let stringNine = "9";
+
+// console.log(numberNine + numberNine);
+// console.log(stringNine + stringNine);
+// console.log(stringNine + numberNine);
+
+// let greeting = "Hello ";
+//let name = "Nick";
+// console.log(greeting + name);
+
+// // Length of String
+
+//console.log("Supercalifragilisticexpialidocious".length);
+
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+// console.log(name[3]);
+// console.log(name[4]);
+
+// let codeword1 = "are";
+// let codeword2 = "tubas";
+// let codeword3 = "unsafe";
+// let codeword4 = "?!";
+
+// console.log(codeword1[1] + codeword2[1] + codeword3[1] + codeword4[1]);
+
+
+// //Cutting Strings
+
+//console.log("a string".slice(1,5));
+
+//let longString = "My long string is long";
+//console.log(longString.slice(3,14));
+// console.log(longString.slice(15,17));
+
+// console.log(longString.toUpperCase());
+// console.log(longString.toLowerCase());
+
+// let str = "hELlo THERE, hOW ARE yOu doINg?";
+// console.log(str[0].toUpperCase() + str.slice(1).toLowerCase());
+
+
+//BOOLEANS
+
+//&&
+
+// let hadShower = false;
+// let hasBackpack = false;
+// console.log(hadShower && hasBackpack);
+
+// //Or
+
+// let hasApple = true;
+// let hasOrange = false;
+// console.log(hasApple||hasOrange);
+
+// //Not
+
+// let isWeekend = true;
+// let needToShowerToday = !isWeekend;
+// console.log(needToShowerToday);
+
+// console.log(!needToShowerToday);
+
+// // //Combining Logical Operators
+
+// isWeekend = false;
+// hadShower = true;
+// hasApple = false;
+// hasOrange = true;
+// console.log(!isWeekend && hadShower && (hasApple || hasOrange));
+
+// // //Numbers with Booleans
+
+// let height = 75;
+// let heightRestriction = 70;
+// console.log(height <= heightRestriction);
+
+// let strNum = "5";
+// let num = 5;
+// console.log(strNum === num);
+
+
+// //TRY IT OUT
+
+// let age = 11;
+// let accompanied = true;
+// let ageRestriction = 13;
+// console.log((age>=ageRestriction || accompanied));
+
+// let poll = null;
+// console.log(poll);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
