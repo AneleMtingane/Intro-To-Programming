7421815627ddd0f7170127d4d538fc9312639e90
Anele Mtingane <139566435+4n313@users.noreply.github.com>
2023-11-28 18:34:05 +0200

    Add files via upload

---
+++ Kids Book/Chapter3/Chapter 3.js
@@ -0,0 +1,107 @@
+//Creating Arrays
+
+// let Dinosaurs = ["T-Rex", "Velociraptor", "Stegosaurus", "Triceratops","Brachiosaurus","Pterandon","Apatosaurus","Diplodocus","Compsognathus"];
+// console.log(Dinosaurs);
+
+// // //Accessing Array Elements
+
+// console.log(Dinosaurs[2]);
+
+// // //Setting or Changing Array elements
+
+// Dinosaurs[0] = "Tyrannosaurus Rex";
+// console.log(Dinosaurs);
+
+// Dinosaurs[34] = "Ptectodyl";
+// console.log(Dinosaurs);
+
+// //Mixing Data Types in Arrays
+
+//let DinosAndNums = [3,"dinosaurs",["triceratops","stegosaurus",3627.5],10];
+// console.log(DinosAndNums[2]);
+// console.log(DinosAndNums[2][0]);
+
+// //Array Length
+
+//console.log(DinosAndNums[DinosAndNums.length-1]);
+
+// //Adding Elements to an array
+
+// DinosAndNums.push(3457);
+// console.log(DinosAndNums);
+
+// DinosAndNums.unshift("Monkey");
+// console.log(DinosAndNums);
+
+// DinosAndNums.pop();
+// console.log(DinosAndNums);
+
+// Adding Arrays Together
+
+// let ConcatDinosAndDinosNums = Dinosaurs.concat(DinosAndNums);
+// console.log(ConcatDinosAndDinosNums);
+
+// //Finding Index of an Element In an Array
+
+// console.log(Dinosaurs.indexOf("Velociraptor"));
+
+// // //If Not in Array
+// console.log(Dinosaurs.indexOf("Zebra"));
+
+// //Arrays Into String
+
+// let boringAnimals = ["Monkey", "Cat", "Fish", "Lizard"];
+// console.log(boringAnimals.join());
+
+
+// let landmarks = [];
+// landmarks.push("My house");
+// landmarks.push("Front path");
+// landmarks.push("Flickering streetlamp");
+// landmarks.push("Leaky fire hydrant");
+// landmarks.push("Fire station");
+// landmarks.push("Cat rescue center");
+// landmarks.push("My old school");
+// landmarks.push("My friend's house");
+// console.log(landmarks);
+
+// landmarks.pop();
+// console.log(landmarks);
+
+// console.log(Math.random()*10);
+
+// console.log(Math.floor(3.99999999999));
+
+// console.log(Math.floor(Math.random()*10));
+
+// let randomWords = ["Explosion", "Cave", "Princess", "Pen"];
+// let randomIndex = Math.floor(Math.random() * 4);
+// console.log(randomWords[randomIndex]);
+
+//Programming Challenges
+
+// randomAnimal = ["Dog","Cat","Rabbit","Horse","Chicken","Hamster"];
+// randomAdjective = ["Ugly","Big","Small","Squeaky","Blue"];
+// randomWord = ["cockroach","rat","elephant","snake","baboon"];
+
+// randomInsult = "Your " + randomAnimal[Math.floor(Math.random()*2)] + " is " + randomAdjective[Math.floor(Math.random()*3)] + " like a " + randomWord[Math.floor(Math.random()*4)];
+// console.log(randomInsult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
