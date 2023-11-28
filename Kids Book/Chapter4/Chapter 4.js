dee6e05ceceb5e5a9e8d82b7434434363a7b6790
Anele Mtingane <139566435+4n313@users.noreply.github.com>
2023-11-28 18:35:19 +0200

    Add files via upload

---
+++ Kids Book/Chapter4/Chapter 4.js
@@ -0,0 +1,232 @@
+//Creating Objects
+
+// let cat = {
+//     "legs": 3,
+//     "name": "Harmony",
+//     "color": "Tortoiseshell"
+// };
+// console.log(cat);
+
+// //Keys Without Quotes
+
+// // cat = {
+// //     legs: 3,
+// //     "full name": "Harmony",
+// //     color: "Tortoiseshell"
+// //}
+// // console.log(cat);   
+
+// console.log(cat["name"]);
+
+// // cat = {
+// //     legs: 3,
+// //     "full name": "Harmony Philomena Snuggly-Pants Morgan",
+// //     color: "Tortoiseshell"
+// //    };
+// //    console.log(cat); 
+
+// //Accessing values in objects
+
+// //console.log(catname");  
+// console.log(cat.name);
+
+// console.log(Object.keys(cat));
+
+// let person = {};
+// person["name"] = "Bokang";
+// person.Eyes = "Black";
+// person["Legs"] = 3;
+// console.log(person);
+
+// let dinosaurs = [
+//  { name: "Tyrannosaurus Rex", period: "Late Cretaceous" },
+//  { name: "Stegosaurus", period: "Late Jurassic" },
+//  { name: "Plateosaurus", period: "Triassic" }
+// ];
+
+// console.log(dinosaurs);
+
+// console.log(dinosaurs[1]["period"]);
+
+
+//An Array of Friens
+
+// let anna = { name: "Anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
+// let dave = { name: "Dave", age: 5, luckyNumbers: [3, 9, 40] };
+// let kate = { name: "Kate", age: 9, luckyNumbers: [1, 2, 3] };
+
+// let friends = [anna,dave, kate];
+// console.log(friends);
+
+// console.log(friends[2].age);
+
+// let owedMoney = {};
+// owedMoney["Jimmy"] = 5;
+// owedMoney["Anna"] = 7;
+// owedMoney["Bokang"] = 3795;
+
+// owedMoney["Jinen"];
+
+//    console.log(owedMoney["Jenije"]);
+
+
+// let movies = {
+//  "Finding Nemo": {
+//  releaseDate: 2003,
+//  duration: 100,
+//  actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+//  format: "DVD"
+//  },
+//  "Star Wars: Episode VI - Return of the Jedi": {
+//  releaseDate: 1983,
+//  duration: 134,
+//  actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+//  format: "DVD"
+//  },
+//  "Harry Potter and the Goblet of Fire": {
+//  releaseDate: 2005,
+//  duration: 157,
+//  actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+//  format: "Blu-ray"
+//  }
+// };
+
+// console.log(movies);
+
+
+//Programming Challenge
+
+
+score = {
+
+Bokang: 0,
+
+Anele: 0,
+
+Siya: 0
+
+
+}
+
+score.Siya++;
+score.Bokang++;
+score.Anele++;
+
+
+console.log(score);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+3
+
+// //Adding Values in objects
+// JGYHFGYDTD
+// GYF
